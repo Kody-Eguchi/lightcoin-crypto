9b32f9fb90dea39db9ed797a98fae617d1f46cfb
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-17 20:22:50 -0500

    strech portions are completed

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 class Account {
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (const n of this.transactions) {
+      balance += n.value;
+    }
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 }
 
@@ -12,7 +23,12 @@ class Transaction {
   }
 
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 }
 
@@ -20,11 +36,19 @@ class Deposit extends Transaction {
   get value() {
     return this.amount;
   }
+  isAllowed() {
+    return true;
+  }
 }
 
 class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
+  }
+  isAllowed() {
+    if (this.account.balance - this.amount >= 0) {
+      return true;
+    }
   }
 }
 
@@ -40,4 +64,12 @@ t1.commit();
 const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
 
+const t3 = new Withdrawal(60.00, myAccount);
+t3.commit();
+
+const t4 = new Withdrawal(50.00, myAccount);
+t4.commit();
+
 console.log('Ending Balance:', myAccount.balance);
+
+console.log(myAccount);
